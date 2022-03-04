89fc485d1446093daf190d6e03cca3475050c744
Jesse Braham <jesse@beta7.io>
2022-03-04 15:48:10 -0800

    Add about and 404 pages, various template/styling updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,13 @@ module.exports = {
       colors: {
         "gray-300": "#dbdcdc",
         "gray-400": "#8c8e8f",
-        "gray-600": "#707377",
-        "gray-700": "#414244",
         "gray-800": "#242526",
         "gray-900": "#17181a",
       },
     },
     screens: {
-      "sm": "640px",
-      "md": "768px",
+      "sm": "768px",
+      "md": "960px",
     },
   },
 }
