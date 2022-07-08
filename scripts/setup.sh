8eb705212d65eb9e611034e3b4452bd99135515d
Jesse Braham <jesse@beta7.io>
2022-07-08 07:56:24 -0700

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,6 +9,10 @@ else
     OS="linux"
 fi
 
+if [ "$ARCH" == "x86_64" ]; then
+    ARCH="x64"
+fi
+
 EXE="tailwindcss-$OS-$ARCH"
 
 curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/$EXE"
